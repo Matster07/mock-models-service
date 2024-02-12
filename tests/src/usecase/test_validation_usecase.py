cf9b153914cba1e69c753784d61f5ab805919487
matve <matveystepin07@gmail.com>
2024-02-12 19:57:24 +0300

    Mirror fixes

--- tests/src/usecase/test_validation_usecase.py
+++ tests/src/usecase/test_validation_usecase.py
@@ -1,7 +1,7 @@
 from src.usecase.validation_usecase import ValidationUseCase
 
 
-def test_applying_model_return_false() -> None:
+def test_applied_model_must_return_true() -> None:
     data = "TEST"
 
     validation_use_case = ValidationUseCase()
---
+++ tests/src/user_interface/http/test_routes.py
@@ -0,0 +1,61 @@
+from fastapi.testclient import TestClient
+
+from src.user_interface.http.app import create_application
+
+client = TestClient(app=create_application())
+
+
+def test_validation_route_for_int_input_type():
+    response = client.post(url="/api/v1/text/validate", json={"data": 123})
+    assert response.status_code == 422
+    assert response.json() == {
+        "errors": [
+            {
+                "msg": "Input should be a valid string",
+                "type": "string_type",
+                "input": "123"
+            }
+        ]
+    }
+
+
+def test_validation_route_for_bool_input_type():
+    response = client.post(url="/api/v1/text/validate", json={"data": False})
+    assert response.status_code == 422
+    assert response.json() == {
+        "errors": [
+            {
+                "msg": "Input should be a valid string",
+                "type": "string_type",
+                "input": "False"
+            }
+        ]
+    }
+
+
+def test_validation_route_for_null_input_type():
+    response = client.post(url="/api/v1/text/validate", json={"data": None})
+    assert response.status_code == 422
+    assert response.json() == {
+        "errors": [
+            {
+                "msg": "Input should be a valid string",
+                "type": "string_type",
+                "input": "None"
+            }
+        ]
+    }
+
+
+def test_validation_route_for_empty_input():
+    response = client.post(url="/api/v1/text/validate", json={})
+    assert response.status_code == 422
+    assert response.json() == {
+        "errors": [
+            {
+                "msg": "Field required",
+                "type": "missing",
+                "input": "{}"
+            }
+        ]
+    }
